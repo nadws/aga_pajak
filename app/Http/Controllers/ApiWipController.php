e1c9c11bb9693e45da796d74d7cc327defb612a7
nadws <nandw567@gmail.com>
2024-02-15 14:48:48 +0800

    dsa

--- app/Http/Controllers/ApiWipController.php
+++ app/Http/Controllers/ApiWipController.php
@@ -8,18 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 class ApiWipController extends Controller
 {
-    function bk_pilih(Request $r)
-    {
-        $gudang = GudangBkModel::getSummaryWip();
-        $response = [
-            'status' => 'success',
-            'message' => 'Data Sarang berhasil diambil',
-            'data' => [
-                'gudang' => $gudang
-            ],
-        ];
-        return response()->json($response);
-    }
+
 
     function get_sum_wip(Request $r)
     {
