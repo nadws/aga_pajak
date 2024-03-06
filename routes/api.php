eada15b498a1be7c8edecdcd1dd5e1c3e93913f2
aldmf26 <aldimf26@gmail.com>
2024-03-06 10:19:31 +0800

    ada api sumWip

--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,5 @@ Route::controller(ApiWipController::class)
         Route::get('/', 'bk_pilih')->name('index');
         Route::get('/get_sum_wip', 'get_sum_wip')->name('get_sum_wip');
         Route::get('/bkCetakApi', 'bkCetakApi')->name('bkCetakApi');
+        Route::get('/sumWip', 'sumWip')->name('sumWip');
     });
