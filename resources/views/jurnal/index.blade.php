a7ff3d41b37e4840732435a61dcc15d618a91897
aldmf26 <aldimf26@gmail.com>
2024-04-19 09:11:29 +0800

    update ada damin di jurnal

--- resources/views/jurnal/index.blade.php
+++ resources/views/jurnal/index.blade.php
@@ -43,6 +43,7 @@
                         <th>Keterangan</th>
                         <th style="text-align: right">Debit</th>
                         <th style="text-align: right">Kredit</th>
+                        <th>Admin</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
@@ -74,6 +75,7 @@
                             @endif
                             <td align="right">{{ number_format($a->debit, 2) }}</td>
                             <td align="right">{{ number_format($a->kredit, 2) }}</td>
+                            <td>{{ strtoupper($a->admin) }}</td>
                             <td>
                                 <div class="btn-group" role="group">
                                     <span class="btn btn-sm" data-bs-toggle="dropdown">
@@ -133,30 +135,26 @@
         </form>
 
 
-        <form action="{{ route('jurnal-delete') }}" method="get">
-            <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div class="row">
-                                <h5 class="text-danger ms-4 mt-4"><i class="fas fa-trash"></i> Hapus Data</h5>
-                                <p class=" ms-4 mt-4">Apa anda yakin ingin menghapus ?</p>
-                                <input type="hidden" class="no_nota" name="no_nota">
-                                <input type="hidden" name="tgl1" value="{{ $tgl1 }}">
-                                <input type="hidden" name="tgl2" value="{{ $tgl2 }}">
-                                <input type="hidden" name="id_proyek" value="{{ $id_proyek }}">
-                            </div>
+        <div class="modal fade" id="delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="row">
+                            <h5 class="text-danger ms-4 mt-4"><i class="fas fa-trash"></i> Hapus Data</h5>
+                            <p class=" ms-4 mt-4">Apa anda yakin ingin menghapus ?</p>
+                            <input type="hidden" class="no_nota" name="no_nota">
+                            <input type="hidden" name="tgl1" value="{{ $tgl1 }}">
+                            <input type="hidden" name="tgl2" value="{{ $tgl2 }}">
+                            <input type="hidden" name="id_proyek" value="{{ $id_proyek }}">
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-outline-danger"
-                                data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn btn-danger">Hapus</button>
                     </div>
                 </div>
             </div>
-        </form>
+        </div>
 
 
         <x-theme.modal title="Detail Jurnal" size="modal-lg-max" idModal="detail">
